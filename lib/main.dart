507d28eeaf9377bc7bd19c7f3ea88669215e0e0e
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-01 18:50:49 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,55 @@ class Home extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.yellow[800]!, Colors.red[300]!],
+            colors: [
+              Colors.yellow[800]!,
+              Colors.yellow[600]!,
+              // Colors.yellow[500]!
+            ],
+            stops: const [0.0, 1.0],
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20), // Remove 'const' here
-            const Text(
-              'Welcome Back',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10), // Remove 'const' here
-            const Text(
-              'Please sign in to your account',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            ElevatedButton(
-              child: const Text('Sign In'),
-              onPressed: () {
-                // TODO: Handle login
-              },
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            const Text(
-              'Forgot Password?',
-              style: TextStyle(fontSize: 16, color: Colors.blue),
-            ),
-          ],
-        ),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     const SizedBox(height: 20), // Remove 'const' here
+        //     const Text(
+        //       'Welcome Back',
+        //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        //     ),
+        //     const SizedBox(height: 10), // Remove 'const' here
+        //     const Text(
+        //       'Please sign in to your account',
+        //       style: TextStyle(fontSize: 16),
+        //     ),
+        //     const SizedBox(height: 20), // Remove 'const' here
+        //     const TextField(
+        //       decoration: InputDecoration(
+        //         labelText: 'Username',
+        //       ),
+        //     ),
+        //     const SizedBox(height: 20), // Remove 'const' here
+        //     const TextField(
+        //       decoration: InputDecoration(
+        //         labelText: 'Password',
+        //       ),
+        //     ),
+        //     const SizedBox(height: 20), // Remove 'const' here
+        //     ElevatedButton(
+        //       child: const Text('Sign In'),
+        //       onPressed: () {
+        //         // TODO: Handle login
+        //       },
+        //     ),
+        //     const SizedBox(height: 20), // Remove 'const' here
+        //     const Text(
+        //       'Forgot Password?',
+        //       style: TextStyle(fontSize: 16, color: Colors.blue),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
