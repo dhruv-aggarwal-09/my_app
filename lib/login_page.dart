ae4473725e7c0a5358ddf1765ba86fb524fe04f5
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-25 20:17:51 +0530

    init

---
+++ lib/login_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.yellow[800]!,
+              Colors.yellow[600]!,
+            ],
+            stops: const [0.0, 1.0],
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/logo.png',
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            Image.asset('assets/image 1.png'),
+            Container(
+              width: 450,
+              height: 300,
+              color: Colors.black,
+              // decoration: BoxDecoration(
+              //   borderRadius: BorderRadius.circular(20),
+              // ),
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                      style: DefaultTextStyle.of(context).style,
+                      children: const <TextSpan>[
+                        TextSpan(
+                            text: 'Welcome ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        TextSpan(
+                          text: 'Back',
+                          style: TextStyle(
+                            color: Colors.yellow,
+                            fontSize: 34,
+                            fontWeight: FontWeight.bold,
+                            decorationStyle: TextDecorationStyle.double,
+                          ),
+                        )
+                      ]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/login_page.dart';
 
 void main() => runApp(const MaterialApp(home: Home()));
 
@@ -7,57 +8,10 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.yellow[800]!,
-              Colors.yellow[600]!,
-              // Colors.yellow[500]!
-            ],
-            stops: const [0.0, 1.0],
-            begin: FractionalOffset.topCenter,
-            end: FractionalOffset.bottomCenter,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              width: 200,
-              height: 200,
-              alignment: Alignment.topCenter,
-            ),
-            const SizedBox(height: 5), // Remove 'const' here
-            Image.asset('assets/image 1.png'),
-            const SizedBox(height: 20), // Remove 'const' here
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            ElevatedButton(
-              child: const Text('Sign In'),
-              onPressed: () {
-                // TODO: Handle login
-              },
-            ),
-            const SizedBox(height: 20), // Remove 'const' here
-            const Text(
-              'Forgot Password?',
-              style: TextStyle(fontSize: 16, color: Colors.blue),
-            ),
-          ],
-        ),
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [LoginScreen()],
       ),
     );
   }
