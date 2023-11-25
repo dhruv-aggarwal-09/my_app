af143c1f02c294a17bcf5eab20e20aafe63c30ea
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-11-25 22:49:01 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:my_app/containers.dart';
 import 'package:flutter/services.dart';
 import 'package:my_app/sidebar_page.dart';
 // import 'package:my_app/login_page.dart';
+// import 'package:my_app/containers.dart';
 
 void main() =>
     runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
--- lib/sidebar_page.dart
+++ lib/sidebar_page.dart
@@ -110,24 +110,345 @@ class SideBar extends StatelessWidget {
                       borderRadius: BorderRadius.circular(1)),
                 ),
               ),
-              child: const Positioned(
-                left: 72,
-                top: 15,
-                child: SizedBox(
-                  width: 93,
-                  height: 43,
-                  child: Text(
-                    'LOFO',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 32,
-                        fontFamily: 'Quicksand',
-                        fontWeight: FontWeight.w700,
-                        height: 0),
-                  ),
+            ),
+            const Positioned(
+              left: 72,
+              top: 15,
+              child: SizedBox(
+                width: 93,
+                height: 43,
+                child: Text(
+                  'LOFO',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32,
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.w700,
+                      height: 0),
                 ),
               ),
             ),
+            // ignore: duplicate_ignore
+            Positioned(
+              left: 21,
+              top: 79,
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: 320,
+                height: 41,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 31,
+                      top: 0,
+                      child: Container(
+                        width: 21,
+                        height: 21,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 320,
+                        height: 41,
+                        decoration: ShapeDecoration(
+                          color: const Color(0xFF616060),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 52,
+                      top: 9,
+                      child: SizedBox(
+                        width: 78,
+                        child: Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6000000238418579),
+                            fontSize: 16,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 19,
+              top: 129,
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: 320,
+                height: 41,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 320,
+                        height: 41,
+                        decoration: ShapeDecoration(
+                            color: const Color(0xFF616060),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ),
+                    Positioned(
+                      left: 11,
+                      top: 7,
+                      child: Container(
+                        width: 26,
+                        height: 26,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(),
+                        child: const Stack(children: []),
+                      ),
+                    ),
+                    Positioned(
+                      left: 54,
+                      top: 10,
+                      child: SizedBox(
+                        width: 78,
+                        child: Text(
+                          'Category',
+                          style: TextStyle(
+                              color:
+                                  Colors.white.withOpacity(0.6000000238418579),
+                              fontSize: 16,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.w700,
+                              height: 0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned(
+              left: 20,
+              top: 179,
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: 320,
+                height: 41,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 320,
+                        height: 41,
+                        decoration: ShapeDecoration(
+                            color: const Color(0xFF616060),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                      ),
+                    ),
+                    Positioned(
+                      left: 54,
+                      top: 10,
+                      child: SizedBox(
+                        width: 108,
+                        child: Text(
+                          'About Us',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6000000238418579),
+                            fontSize: 16,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 9,
+                      top: 7,
+                      // ignore: sized_box_for_whitespace
+                      child: Container(
+                        width: 26,
+                        height: 26,
+                        child: const Stack(
+                          children: [],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 229,
+              child: Container(
+                width: 320,
+                height: 41,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 320,
+                        height: 41,
+                        decoration: ShapeDecoration(
+                            color: const Color(0xFF616060),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                      ),
+                    ),
+                    Positioned(
+                      left: 52,
+                      top: 10,
+                      child: SizedBox(
+                        width: 108,
+                        child: Text(
+                          'Privacy Policy',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6000000238418579),
+                            fontSize: 16,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 9,
+                      top: 7,
+                      // ignore: sized_box_for_whitespace
+                      child: Container(
+                        width: 26,
+                        height: 26,
+                        child: const Stack(
+                          children: [],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 279,
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: 320,
+                height: 41,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 320,
+                        height: 41,
+                        decoration: ShapeDecoration(
+                            color: const Color(0xFF616060),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                      ),
+                    ),
+                    Positioned(
+                      left: 52,
+                      top: 10,
+                      child: SizedBox(
+                        width: 108,
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6000000238418579),
+                            fontSize: 16,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 9,
+                      top: 7,
+                      child: Container(
+                        width: 26,
+                        height: 26,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: 2.17,
+                              top: 2.17,
+                              // ignore: sized_box_for_whitespace
+                              child: Container(
+                                width: 21.67,
+                                height: 21.67,
+                                child: const Stack(
+                                  children: [],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 160,
+              top: 603,
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: 88,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // ignore: sized_box_for_whitespace
+                            Container(
+                              width: 12.50,
+                              height: 12.50,
+                              child: const Stack(children: []),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ]),
         ));
   }
